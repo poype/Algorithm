7f41a8cc3c15ed3a6ff0710a9aefba3798ca3901
poype <253186926@qq.com>
2023-11-04 09:45:05 +0800

    最长回文字符串(二)，虽然还是用递归，但利用了一个二维数组缓存结果，提高了性能

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -1,10 +1,19 @@
+# https://leetcode.cn/problems/longest-palindromic-substring/
+
 class Solution(object):
+
+    def __init__(self):
+        # 定义一个类成员变量
+        self.matrix = None
+
     def longestPalindrome(self, s):
         """
         :type s: str
         """
         max_length = 0
         result = ""
+        # init matrix
+        self.matrix = [[None for i in range(len(s))] for j in range(len(s))]
         for i in range(len(s)):
             for j in range(len(s) - 1, -1, -1):
                 if self.is_palindrome(s, i, j) and j - i + 1 > max_length:
@@ -21,18 +30,27 @@ class Solution(object):
         :param end: 终止索引
         :return: 回文字符串返回True，否则返回False
         """
+        if self.matrix[start][end] is not None:
+            return self.matrix[start][end]
+
         if start == end:
+            self.matrix[start][end] = True
             return True
         elif start > end:
+            self.matrix[start][end] = False
             return False
 
         if s[start] != s[end]:
+            self.matrix[start][end] = False
             return False
         elif end - start == 1:
+            self.matrix[start][end] = True
             return True
         else:
-            return self.is_palindrome(s, start + 1, end - 1)
+            check_result = self.is_palindrome(s, start + 1, end - 1)
+            self.matrix[start][end] = check_result
+            return check_result
 
 
 s = Solution()
-print(s.longestPalindrome("abbbaddddddddddda"))
+print(s.longestPalindrome("jcwwnkwiajicysmdueefqjnrokunucidhgkswbgjkkrujkxkxeanrpjvpliomxztalhmvcldnqmkslkprhgtwlnsnygbzdvtdbsdzsdjggzgmhogknpfhtgjmclrkgfqdbiagwrqqcnagosnqrnpapxfrtrhzlyhszdtgkqggmewqmwugrbufiwfvtjhczqgcwpcyjioeacggiwyinpkyxrpxhglrtojgjmmswxnvirfsrbhmpqgjyyagjqfwkqkjkumywvgfutmiwihwnnhbfxcijaoiyxbdnrckexqfhsmmxflaneccyaoqoxfbaylcvvpfafsikebzcdufvhluldguwsmrtjaljpcjestranfrvgvytozxpcvnwquhnsxlmzkehwopgxvifajmrlwqiqylgxibnypxwpkggxevyfoxywfsfpjbzfxxysgxgwxrzkwdqlkrpajlltzqfqshdokibakkhydizsgwbygqulljqmtxkwepitsukwjlrrmsjptwabtlqytprkkuxtqdmptctkfabxsohrfrqvrbjfbppfkpthosveoppiywkkuoasefviegormlqkqlbhnhblkmglxcbszblfipsyumcrjrkrnzplkveznbtdbtlcptgswhiqsjugqrvujkzuwvoxbjremyxqqzxkgerhgtidsefyemtmstsznvgohexdgetqbhrxaomzsamapxhjibfvtbquhowyrwyxthpwvmfyyqsyibemnfbwkddtyoijzwfxhossylygxmnznpegtgvlrsreepkrcdgbujkghrgtsxwlvxrgrqxnvgqkppbkrxjupjfjcsfzepdemaulfetn"))
