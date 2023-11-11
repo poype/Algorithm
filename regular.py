903b5f2ad0782532366aaaa41cc5342ce5bc460a
poype <253186926@qq.com>
2023-11-11 22:39:21 +0800

    正则表达式匹配(二)
    增加了缓存dp矩阵还是time limit。但结果应该是正确的

--- regular.py
+++ regular.py
@@ -1,17 +1,28 @@
 # https://leetcode.cn/problems/regular-expression-matching/
 
 class Solution(object):
+    def __init__(self):
+        self.dp = None
     def isMatch(self, s, p):
+        # 这里注意矩阵的大小，要用len(p) + 1 和 len(s) + 1
+        # 还要注意矩阵的方向不要搞反
+        self.dp = [[None for j in range(len(p) + 1)] for i in range(len(s) + 1)]
         return self.is_match(s, p, 0, 0)
 
     def is_match(self, s, p, i, j):
+        if self.dp[i][j] is not None:
+            return self.dp[i][j]
+
         if i == len(s) and j == len(p):
+            self.dp[i][j] = True
             return True
 
         if i < len(s) and j == len(p):
+            self.dp[i][j] = False
             return False
 
         if i == len(s) and (j + 1 >= len(p) or p[j + 1] != '*'):
+            self.dp[i][j] = False
             return False
 
         if j + 1 < len(p) and p[j + 1] == '*':
@@ -25,8 +36,9 @@ class Solution(object):
         if s[i] == p[j] or p[j] == '.':
             return self.is_match(s, p, i + 1, j + 1)
         else:
+            self.dp[i][j] = False
             return False
 
 
 s = Solution()
-print(s.isMatch("ab", ".*c"))
+print(s.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*c"))
