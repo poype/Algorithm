528cf6a5cd0229af776ce1de79e4d6b84b581987
poype <253186926@qq.com>
2023-12-08 21:15:43 +0800

    45. 跳跃游戏 II 上一个commit会超时，增加flags数组用于缓存结果

--- jump2.py
+++ jump2.py
@@ -3,17 +3,27 @@
 from typing import List
 
 class Solution(object):
+    def __init__(self):
+        self.flags = []
+
     def jump(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return 0
 
+        self.flags = [-1 for i in range(len(nums))]
+
         return self.jump_from_start(nums, 0)
 
     def jump_from_start(self, nums: List[int], start: int) -> int:
+        if self.flags[start] > 0:
+            return self.flags[start]
+
         if nums[start] >= len(nums) - start - 1:
+            self.flags[start] = 1
             return 1
 
         if nums[start] == 0:
+            self.flags[start] = 2 ** 32 - 2
             return 2 ** 32 - 2
 
         min_steps = 2 ** 32 - 2
@@ -23,6 +33,7 @@ class Solution(object):
             if steps < min_steps:
                 min_steps = steps
 
+        self.flags[start] = min_steps + 1
         return min_steps + 1
 
 s = Solution()
