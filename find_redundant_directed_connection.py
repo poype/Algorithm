5bf5cf1b851852f7bb4c0e877a21a41945b1901c
poype <253186926@qq.com>
2023-12-15 08:20:40 +0800

    685. 冗余连接 II

--- find_redundant_directed_connection.py
+++ find_redundant_directed_connection.py
@@ -1,7 +1,12 @@
-# https://leetcode.cn/problems/redundant-connection-ii/
+# https://leetcode.cn/problems/redundant-connection/description/
 
-from typing import List
+from typing import List, Set
 
+
+# 此题虽然是有向图，但要把它当成无向图来看待
+# 也是先在无向图中找到一个环，然后删除环中的一个边，但要注意的是，如果环中存在某个节点的入度大于1，则一定要删除与那个节点的边
+# 此外要特殊处理 a -> b and a <- b 这种互相指向的case，因为判断环的算法无法找出这种环
+# 所以此题基本上是在 684. 冗余连接 的基础上的微调
 
 class Node(object):
     def __init__(self, num: int):
@@ -11,54 +16,45 @@ class Node(object):
     def add_edge(self, edge):
         self.edges.add(edge)
 
+
 class Solution:
     def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
+        n = len(edges)  # 题目中说明是比树多一条边，所以节点数量与边的数量相同
 
         node_list = []
         for i in range(n + 1):
             node_list.append(Node(i))
 
-        # 构造图
         in_degree_list = [0 for _ in range(n + 1)]
+        # 构造图
         for edge in edges:
             node_list[edge[0]].add_edge(node_list[edge[1]])
+            node_list[edge[1]].add_edge(node_list[edge[0]])
             in_degree_list[edge[1]] += 1
 
         in_degree_lg_1_node = -1
-        in_degree_eq_0_node = -1
         # 存在一个入度为0的node，证明没有环
         for i in range(1, n + 1):
-            if in_degree_list[i] == 0:
-                in_degree_eq_0_node = i
             if in_degree_list[i] > 1:
                 in_degree_lg_1_node = i
 
-        if in_degree_lg_1_node != -1:
-            for j in range(len(edges) - 1, -1, -1):
-                if edges[j][1] == in_degree_lg_1_node and edges[j][0] != in_degree_eq_0_node:
-                    return edges[j]
-
-        if in_degree_eq_0_node != -1:
-            for j in range(len(edges) - 1, -1, -1):
-                if edges[j][0] != in_degree_eq_0_node:  # 如果存在入度为0的节点，除了与该节点相关联的边外，其它的边随便删一个
-                    return edges[j]
-
-        # 不存在入度为0的node，图中存在环
+        # 下面dfs遍历去找环
+        none_node = Node(-1)  # 标记node，代表None
         for i in range(n):
             trace = []
-            if self.dfs(node_list[i], trace):
+            if self.dfs(node_list[i], none_node, trace):
                 last_trace = trace.pop()
                 while trace[0] != last_trace:
                     trace.remove(trace[0])
+                return self.find_last_edge(trace, edges, in_degree_lg_1_node)
 
-                for k in range(len(trace)):  # 入度大于1的点一定
-                    if in_degree_list[trace[k]] > 1:
-                        return [trace[k - 1], trace[k]]
+        for i in range(len(edges)):
+            for j in range(i, len(edges)):
+                if edges[i][0] == edges[j][1] and edges[i][1] == edges[j][0]:
+                    trace = edges[i][:]
+                    return self.find_last_edge(trace, edges, in_degree_lg_1_node)
 
-                return self.find_last_edge(trace, edges)
-
-    def dfs(self, node: Node, trace: List) -> bool:
+    def dfs(self, node: Node, from_node: Node, trace: List) -> bool:
         if trace.__contains__(node.num):
             trace.append(node.num)
             return True
@@ -66,13 +62,16 @@ class Solution:
         trace.append(node.num)
 
         for edge_node in node.edges:
-            if self.dfs(edge_node, trace):
+            if edge_node == from_node:  # 由于是无向图，要确保不能走回头路
+                continue
+
+            if self.dfs(edge_node, node, trace):
                 return True
 
         trace.remove(node.num)
         return False
 
-    def find_last_edge(self, trace: List, edges: List[List[int]]):
+    def find_last_edge(self, trace: List, edges: List[List[int]], in_degree_lg_1_node: int):
         max_pos = -1
         result = [0, 0]
         for i in range(len(trace)):
@@ -80,13 +79,20 @@ class Solution:
                 trace_edge = [trace[i], trace[0]]
             else:
                 trace_edge = trace[i:i + 2]
+            trace_edge.sort()
             for j in range(len(edges)):
-                if trace_edge[0] == edges[j][0] and trace_edge[1] == edges[j][1] and j > max_pos:
+                edge = edges[j][:]
+                edge.sort()
+                if trace_edge[0] == edge[0] and trace_edge[1] == edge[1] and j > max_pos:
+                    if in_degree_lg_1_node != -1 and edges[j][1] != in_degree_lg_1_node:  # 一旦有入度大于1的节点，则必须删除与那个节点相关的边
+                        continue
+
                     max_pos = j
-                    result[0] = trace_edge[0]
-                    result[1] = trace_edge[1]
+                    result[0] = edges[j][0]
+                    result[1] = edges[j][1]
+
         return result
 
 
 s = Solution()
-print(s.findRedundantDirectedConnection([[4,2],[1,5],[5,2],[5,3],[2,4]]))
+print(s.findRedundantDirectedConnection([[4, 2], [1, 5], [5, 2], [5, 3], [2, 4]]))
