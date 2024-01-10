2dcbe099a72c0cca3d06a5422267caf30a9a7d36
poype <253186926@qq.com>
2024-01-10 10:31:51 +0800

    189. 轮转数组

--- rotate2.py
+++ rotate2.py
@@ -12,9 +12,17 @@ class Solution:
         if k == 0:
             return
 
-        for i in range(k):
-            self.move_right(nums)
+        n = len(nums)
+        left_slice = nums[0: n - k]
 
+        # 直接将后k个元素copy到数组前部
+        for i in range(k):
+            nums[i] = nums[n - k + i]
+
+        for i in range(k, n):
+            nums[i] = left_slice[i - k]
+
+    # 下面这个方法无用了
     def move_right(self, nums: List[int]):
         last_element = nums[-1]
         for i in range(len(nums) - 2, -1, -1):
