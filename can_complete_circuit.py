c291a5285da33a10782e88929e6ed3eaaaab7906
poype <253186926@qq.com>
2024-01-14 12:06:18 +0800

    134. 加油站  成功通过

--- can_complete_circuit.py
+++ can_complete_circuit.py
@@ -4,11 +4,42 @@ from typing import List
 
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        for i in range(len(gas)):
-            if self.can_complete_from_start(i, gas, cost):
+        i = 0
+        while i < len(gas):
+            pos = self.move_farthest_pos(i, gas, cost)
+            if pos == i:
                 return i
 
+            if pos > i:
+                i = pos
+            else:
+                return -1  # 这已经能够缺点找不到结果了
+
         return -1
+
+    def move_farthest_pos(self, start_index: int, gas: List[int], cost: List[int]) -> int:
+        """
+        从start_index开始，可以走到最远的位置
+        :return: 可以走到最远的位置， 下一次会直接从这个点向后搜索
+        """
+        n = len(gas)
+
+        i = start_index
+        gas_volume = gas[i]
+        if gas_volume < cost[i]:
+            return start_index + 1
+
+        gas_volume -= cost[i]
+        i = (i + 1) % n
+
+        while i != start_index:
+            gas_volume += gas[i]
+            if gas_volume < cost[i]:
+                return i
+            gas_volume -= cost[i]
+            i = (i + 1) % n
+        return start_index
+
 
     def can_complete_from_start(self, start_index: int, gas: List[int], cost: List[int]) -> bool:
         n = len(gas)
@@ -31,7 +62,7 @@ class Solution:
 
 
 s = Solution()
-gas = [1, 2, 3, 4, 5]
-cost = [3, 4, 5, 1, 2]
+gas = [2, 3, 4]
+cost = [3, 4, 3]
 
 print(s.canCompleteCircuit(gas, cost))
