572971871b38f716a9b01a9cba6dc9143e2a23f1
poype <253186926@qq.com>
2024-01-20 15:03:34 +0800

    76. 最小覆盖子串 成功，避免了每次都构建s_ch_cnt 和 t_ch_cnt

--- min_window.py
+++ min_window.py
@@ -1,64 +1,75 @@
 # https://leetcode.cn/problems/minimum-window-substring/?envType=study-plan-v2&envId=top-interview-150
 
 class Solution:
+    def __init__(self):
+        # key是字符，value是该字符的数量
+        self.s_ch_cnt = {}
+        self.t_ch_cnt = {}
+
     def minWindow(self, s: str, t: str) -> str:
         s_n, t_n = len(s), len(t)
-        if s_n == t_n and self.contain_all_chars(s, t):
-            return s
-
         start, end = 0, t_n
+        self.init_ch_cnt_dict(s[start: end], t)
+
+        if s_n == t_n and self.contain_all_chars():
+            return s
 
         min_len = s_n + 1
         min_s = ""
         for start in range(0, s_n - t_n + 1):
-            if self.contain_all_chars(s[start: end], t):
+            if self.contain_all_chars():
                 if end - start < min_len :
                     min_len = end - start
                     min_s = s[start: end]
+                self.s_ch_cnt[s[start]] -= 1
                 continue
 
             while end < s_n:
+                self.add_ch_to_s_dict(s[end])
                 end += 1
-                if self.contain_all_chars(s[start: end], t):
+                if self.contain_all_chars():
                     if end - start < min_len:
                         min_len = end - start
                         min_s = s[start: end]
                     break
 
+            self.s_ch_cnt[s[start]] -= 1
         return min_s
 
-    def contain_all_chars(self, s: str, t:str):
-        """
-        :return: s字符串中包含t字符串中的所有字符返回True，否则返回False
-        """
+    def add_ch_to_s_dict(self, ch):
+        if ch not in self.s_ch_cnt:
+            self.s_ch_cnt[ch] = 0
+        self.s_ch_cnt[ch] += 1
+
+    def init_ch_cnt_dict(self, s: str, t: str):
         s_n = len(s)
         t_n = len(t)
 
-        # key是字符，value是该字符的数量
-        s_ch_cnt = {}
-        t_ch_cnt = {}
-
         for i in range(s_n):
-            if s[i] not in s_ch_cnt:
-                s_ch_cnt[s[i]] = 0
-            s_ch_cnt[s[i]] += 1
+            if s[i] not in self.s_ch_cnt:
+                self.s_ch_cnt[s[i]] = 0
+            self.s_ch_cnt[s[i]] += 1
 
         for i in range(t_n):
-            if t[i] not in t_ch_cnt:
-                t_ch_cnt[t[i]] = 0
-            t_ch_cnt[t[i]] += 1
+            if t[i] not in self.t_ch_cnt:
+                self.t_ch_cnt[t[i]] = 0
+            self.t_ch_cnt[t[i]] += 1
 
-        for t_key in t_ch_cnt.keys():
-            if t_key not in s_ch_cnt:
+    def contain_all_chars(self):
+        """
+        :return: s字符串中包含t字符串中的所有字符返回True，否则返回False
+        """
+        for t_key in self.t_ch_cnt.keys():
+            if t_key not in self.s_ch_cnt:
                 return False
 
-            if s_ch_cnt[t_key] < t_ch_cnt[t_key]:
+            if self. s_ch_cnt[t_key] < self.t_ch_cnt[t_key]:
                 return False
 
         return True
 
 
 solution = Solution()
-s = "abc"
-t = "ac"
+s = "a"
+t = "a"
 print(solution.minWindow(s, t))
