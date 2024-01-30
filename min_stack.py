b922d7629d2c639909521b369a39db347a5e8123
poype <253186926@qq.com>
2024-01-30 08:06:56 +0800

    155. 最小栈 去掉了对ordered_list的排序，性能更好

--- min_stack.py
+++ min_stack.py
@@ -6,19 +6,20 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        self.ordered_list.append(val)
-        self.ordered_list.sort()
+        if len(self.ordered_list) > 0:
+            top_val = self.ordered_list[len(self.ordered_list) - 1]
+            self.ordered_list.append(min(top_val, val))
+        else:
+            self.ordered_list.append(val)
 
     def pop(self) -> None:
         if len(self.stack) > 0:
-            top_val = self.top()
             self.stack.pop()
-            self.ordered_list.remove(top_val)
-            self.ordered_list.sort()
+            self.ordered_list.pop()
 
     def top(self) -> int:
         top_idx = len(self.stack) - 1
         return self.stack[top_idx]
 
     def getMin(self) -> int:
-        return self.ordered_list[0]
+        return self.ordered_list[len(self.ordered_list) - 1]
