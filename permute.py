a579837d2f5a5fb496d9b357766d020363cc753e
poype <253186926@qq.com>
2024-02-07 09:04:26 +0800

    46. 全排列 改进了之前的code

--- permute.py
+++ permute.py
@@ -2,9 +2,27 @@
 
 from typing import List
 
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.permute_from_start(nums, 0)
+
+        return self.__permute_from_start_(nums, 0)
+
+    def __permute_from_start_(self, nums: List[int], start: int) -> List[List[int]]:
+        result = []
+
+        if start == len(nums) - 1:
+            result.append([nums[start]])
+            return result
+
+        part_result = self.__permute_from_start_(nums, start + 1)
+        for num_list in part_result:
+            for i in range(len(num_list) + 1):
+                new_num_list = num_list[:]
+                new_num_list.insert(i, nums[start])
+                result.append(new_num_list)
+
+        return result
 
     def permute_from_start(self, nums: List[int], start: int) -> List[List[int]]:
         result = []
@@ -38,4 +56,4 @@ class Solution:
 
 
 s = Solution()
-print(s.permute([1, 0]))
+print(s.permute([1, 0, 2]))
