ab9dccb38a80dee31ae358249f1d9a2c5027965f
poype <253186926@qq.com>
2024-02-11 22:20:49 +0800

    63. 不同路径 II success

--- unique_paths_with_obstacles.py
+++ unique_paths_with_obstacles.py
@@ -1,5 +1,5 @@
 # https://leetcode.cn/problems/unique-paths-ii/?envType=study-plan-v2&envId=top-interview-150
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
@@ -7,9 +7,12 @@ class Solution:
         if obstacleGrid[0][0] == 1:
             return 0
 
-        return self.__walk__(obstacleGrid, 0, 0)
+        n, m = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[None for _ in range(m)] for _ in range(n)]
 
-    def __walk__(self, grid: List[List[int]], row: int, column: int) -> int:
+        return self.__walk__(obstacleGrid, 0, 0, dp)
+
+    def __walk__(self, grid: List[List[int]], row: int, column: int, dp: List[List[Optional[int]]]) -> int:
         if (row == len(grid) - 1 and column == len(grid[0]) - 1) and grid[len(grid) - 1][len(grid[0]) - 1] == 0:
             return 1
 
@@ -18,17 +21,21 @@ class Solution:
                 grid[len(grid) - 1][len(grid[0]) - 1] == 0):
             return 1
 
+        if dp[row][column] is not None:
+            return dp[row][column]
+
         path_cnt = 0
         if row < len(grid) - 1 and grid[row + 1][column] == 0:
-            path_cnt += self.__walk__(grid, row + 1, column)
+            path_cnt += self.__walk__(grid, row + 1, column, dp)
 
         if column < len(grid[0]) - 1 and grid[row][column + 1] == 0:
-            path_cnt += self.__walk__(grid, row, column + 1)
+            path_cnt += self.__walk__(grid, row, column + 1, dp)
 
+        dp[row][column] = path_cnt
         return path_cnt
 
 
 s = Solution()
-obstacleGrid = [[0,0],[1,1],[0,0]]
+obstacleGrid = [[0,0],[1,0]]
 
 print(s.uniquePathsWithObstacles(obstacleGrid))
