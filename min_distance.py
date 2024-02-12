a6437d1df62ec5c1c3eb86719855a07e553eb383
poype <253186926@qq.com>
2024-02-12 09:27:31 +0800

    72. 编辑距离

--- min_distance.py
+++ min_distance.py
@@ -1,4 +1,5 @@
 # https://leetcode.cn/problems/edit-distance/?envType=study-plan-v2&envId=top-interview-150
+from typing import Dict
 
 
 class Solution:
@@ -7,9 +8,10 @@ class Solution:
         此题中描述包括插入一个字符、删除一个字符、替换一个字符三种操作，但删除和插入本质上属于同一种操作
         所以只需要考虑删除、替换两种操作即可
         """
-        return self.__min_distance__(word1, word2)
+        cache = {}
+        return self.__min_distance__(word1, word2, cache)
 
-    def __min_distance__(self, word1: str, word2: str) -> int:
+    def __min_distance__(self, word1: str, word2: str, cache: Dict) -> int:
         l1, l2 = len(word1), len(word2)
 
         if l1 == 0:
@@ -17,15 +19,19 @@ class Solution:
         if l2 == 0:
             return l1
 
-        if word1[l1 - 1] == word2[l2 - 1]:
-            return self.__min_distance__(word1[:l1 - 1], word2[:l2 - 1])
+        if f"{word1}_{word2}" in cache:
+            return cache[f"{word1}_{word2}"]
 
-        return 1 + min(self.__min_distance__(word1[:l1 - 1], word2[:l2 - 1]),
-                       self.__min_distance__(word1[:l1 - 1], word2),
-                       self.__min_distance__(word1, word2[:l2 - 1]))
+        if word1[l1 - 1] == word2[l2 - 1]:
+            cache[f"{word1}_{word2}"] = self.__min_distance__(word1[:l1 - 1], word2[:l2 - 1], cache)
+        else:
+            cache[f"{word1}_{word2}"] = 1 + min(self.__min_distance__(word1[:l1 - 1], word2[:l2 - 1], cache),
+                                                self.__min_distance__(word1[:l1 - 1], word2, cache),
+                                                self.__min_distance__(word1, word2[:l2 - 1], cache))
+        return cache[f"{word1}_{word2}"]
 
 
 s = Solution()
-word1 = "intention"
-word2 = "execution"
+word1 = "dinitrophenylhydrazine"
+word2 = "benzalphenylhydrazone"
 print(s.minDistance(word1, word2))
