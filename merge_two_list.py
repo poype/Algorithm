989c8eec8525f18d9701d4e5a8b3b61b94e77665
poype <253186926@qq.com>
2024-03-07 07:57:26 +0800

    优化

--- merge_two_list.py
+++ merge_two_list.py
@@ -9,24 +9,25 @@ class ListNode(object):
 
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        p1, p2 = list1, list2
         head = ListNode()
         p = head
 
-        while p1 is not None and p2 is not None:
-            if p1.val <= p2.val:
-                p.next = p1
-                p = p1
-                p1 = p1.next
+        while list1 is not None and list2 is not None:
+            if list1.val < list2.val:
+                p.next = list1
+                list1 = list1.next
             else:
-                p.next = p2
-                p = p2
-                p2 = p2.next
+                p.next = list2
+                list2 = list2.next
+            p = p.next
 
-        if p1 is None:
-            p1 = p2
+        if list1 is None:
+            list1 = list2
 
-        p.next = p1
+        while list1 is not None:
+            p.next = list1
+            list1 = list1.next
+            p = p.next
 
         return head.next
 
