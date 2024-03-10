bbfa3dd4fcc873c5f5c061af5715eb112150af12
poype <253186926@qq.com>
2024-03-11 07:50:04 +0800

    优化

--- build_tree.py
+++ build_tree.py
@@ -11,47 +11,28 @@ class TreeNode:
 
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        return self.__build_tree__(preorder, inorder)
+
+    def __build_tree__(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if len(preorder) == 0:
             return None
 
-        if len(preorder) == 1:
-            return TreeNode(preorder[0])
+        root = TreeNode(preorder[0])
+        i = inorder.index(preorder[0])
 
-        if len(preorder) == 2:
-            root_node = TreeNode(preorder[0])
-            if preorder[0] == inorder[1]:
-                left_node = TreeNode(inorder[0])
-                root_node.left = left_node
-                return root_node
-            else:
-                right_node = TreeNode(inorder[1])
-                root_node.right = right_node
-                return root_node
+        left_inorder = inorder[0:i]
+        right_inorder = inorder[i + 1:]
 
-        root = preorder[0]
-        root_index_of_inorder = inorder.index(root)
-        left_tree_inorder = inorder[:root_index_of_inorder]
+        j = 1
+        while j < len(preorder) and preorder[j] not in right_inorder:
+            j += 1
 
-        right_tree_inorder = []
-        if root_index_of_inorder < len(inorder) - 1:
-            right_tree_inorder = inorder[root_index_of_inorder + 1:]
+        left_preorder = preorder[1:j]
+        right_preorder = preorder[j:]
 
-        i = 1
-        while i < len(preorder):
-            if right_tree_inorder.__contains__(preorder[i]):
-                break
-            i += 1
+        left_tree = self.__build_tree__(left_preorder, left_inorder)
+        right_tree = self.__build_tree__(right_preorder, right_inorder)
 
-        left_tree_preorder = preorder[1: i]
-        right_tree_preorder = []
-        if i < len(preorder):
-            # 确实存在right_tree
-            right_tree_preorder = preorder[i:]
-
-        root = TreeNode(root)
-        left_tree = self.buildTree(left_tree_preorder, left_tree_inorder)
-        right_tree = self.buildTree(right_tree_preorder, right_tree_inorder)
         root.left = left_tree
         root.right = right_tree
-
         return root
