d48aee94fc2b7151c85f12cadb7a41cb5242f006
poype <253186926@qq.com>
2024-03-16 13:26:56 +0800

    again

--- can_finish.py
+++ can_finish.py
@@ -2,56 +2,68 @@
 
 from typing import List, Set
 
+
 # DFS版本实现
 
 class GraphNode(object):
-    def __init__(self, course: int):
-        self.course = course
-        self.post_courses = []
+    def __init__(self, val: int):
+        self.val = val
+        self.neighbors = []
+        self.in_degree = 0
 
-    def add_post_course(self, course_node):
-        self.post_courses.append(course_node)
+    def add_neighbor(self, neighbor_node: 'GraphNode'):
+        self.neighbors.append(neighbor_node)
+
 
 class Solution:
     def __init__(self):
-        self.dfs_help_list = None  # 为了避免重复计算
+        self.graph = {1: GraphNode(1)}
+        self.visited = []
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        self.dfs_help_list = [None for _ in range(numCourses)]
+        self.visited = [None for _ in range(numCourses + 1)]
+        for item in prerequisites:
+            self.__build_relation__(item[0], item[1])
 
-        course_list = []
-        for i in range(numCourses):
-            course_list.append(GraphNode(i))
-
-        for relation in prerequisites:  # 构造图
-            pre_course = course_list[relation[0]]
-            post_course = course_list[relation[1]]
-            pre_course.add_post_course(post_course)
-
-        for i in range(numCourses):
-            if self.dfs(course_list[i], set()):
+        for graph_node in self.graph.values():
+            if self.__dfs__(graph_node, []):
                 return False
         return True
 
-    def dfs(self, course_node: GraphNode, visited: Set) -> bool:   # 方法参数的类型是GraphNode
-        """遇到访问过的节点就证明有环，有环返回True"""
-        if self.dfs_help_list[course_node.course] is not None:
-            return self.dfs_help_list[course_node.course]
+    def __dfs__(self, node: GraphNode, trace_stack: List[int]) -> bool:
+        """
+        :return: 有环返回True，无环返回False
+        """
+        if self.visited[node.val] is not None:
+            return self.visited[node.val]
 
-        if visited.__contains__(course_node.course):
-            self.dfs_help_list[course_node.course] = True
+        if node.val in trace_stack:
+            self.visited[node.val] = True
             return True
-        visited.add(course_node.course)
 
-        for post_course in course_node.post_courses:
-            if self.dfs(post_course, visited):
-                self.dfs_help_list[course_node.course] = True
+        self.visited.append(node.val)
+        trace_stack.append(node.val)
+        for neighbor_node in node.neighbors:
+            if self.__dfs__(neighbor_node, trace_stack):
+                self.visited[node.val] = True
                 return True
-
-        visited.remove(course_node.course)  # 注意这里还要remove节点，这个节点已经访问过了，要为访问下一个节点做准备
-        self.dfs_help_list[course_node.course] = False
+        trace_stack.pop()
+        self.visited[node.val] = False
         return False
 
+    def __build_relation__(self, val1: int, val2: int):
+        node1 = self.__get_or_create_node__(val1)
+        node2 = self.__get_or_create_node__(val2)
+        node1.add_neighbor(node2)
+        node2.in_degree += 1
 
+    def __get_or_create_node__(self, val: int):
+        if val not in self.graph:
+            self.graph[val] = GraphNode(val)
+        return self.graph[val]
+
+
+numCourses = 20
+prerequisites = [[0,10],[3,18],[5,5],[6,11],[11,14],[13,1],[15,1],[17,4]]
 s = Solution()
-print(s.canFinish(100, [[1,0],[2,0],[2,1],[3,1],[3,2],[4,2],[4,3],[5,3],[5,4],[6,4],[6,5],[7,5],[7,6],[8,6],[8,7],[9,7],[9,8],[10,8],[10,9],[11,9],[11,10],[12,10],[12,11],[13,11],[13,12],[14,12],[14,13],[15,13],[15,14],[16,14],[16,15],[17,15],[17,16],[18,16],[18,17],[19,17],[19,18],[20,18],[20,19],[21,19],[21,20],[22,20],[22,21],[23,21],[23,22],[24,22],[24,23],[25,23],[25,24],[26,24],[26,25],[27,25],[27,26],[28,26],[28,27],[29,27],[29,28],[30,28],[30,29],[31,29],[31,30],[32,30],[32,31],[33,31],[33,32],[34,32],[34,33],[35,33],[35,34],[36,34],[36,35],[37,35],[37,36],[38,36],[38,37],[39,37],[39,38],[40,38],[40,39],[41,39],[41,40],[42,40],[42,41],[43,41],[43,42],[44,42],[44,43],[45,43],[45,44],[46,44],[46,45],[47,45],[47,46],[48,46],[48,47],[49,47],[49,48],[50,48],[50,49],[51,49],[51,50],[52,50],[52,51],[53,51],[53,52],[54,52],[54,53],[55,53],[55,54],[56,54],[56,55],[57,55],[57,56],[58,56],[58,57],[59,57],[59,58],[60,58],[60,59],[61,59],[61,60],[62,60],[62,61],[63,61],[63,62],[64,62],[64,63],[65,63],[65,64],[66,64],[66,65],[67,65],[67,66],[68,66],[68,67],[69,67],[69,68],[70,68],[70,69],[71,69],[71,70],[72,70],[72,71],[73,71],[73,72],[74,72],[74,73],[75,73],[75,74],[76,74],[76,75],[77,75],[77,76],[78,76],[78,77],[79,77],[79,78],[80,78],[80,79],[81,79],[81,80],[82,80],[82,81],[83,81],[83,82],[84,82],[84,83],[85,83],[85,84],[86,84],[86,85],[87,85],[87,86],[88,86],[88,87],[89,87],[89,88],[90,88],[90,89],[91,89],[91,90],[92,90],[92,91],[93,91],[93,92],[94,92],[94,93],[95,93],[95,94],[96,94],[96,95],[97,95],[97,96],[98,96],[98,97],[99,97]]))
+print(s.canFinish(numCourses, prerequisites))
