a5bd56d950d037a9d8acf9442d6b0454da6ddf71
poype <253186926@qq.com>
2024-03-16 19:10:59 +0800

    优化

--- ladder_length.py
+++ ladder_length.py
@@ -1,85 +1,85 @@
 # https://leetcode.cn/problems/word-ladder/
-from typing import List, Set
+from typing import List, Dict
 
 
 class GraphNode:
     def __init__(self, val: str):
         self.val = val
-        self.neighbors = set()
+        self.neighbors: List['GraphNode'] = []
 
-    def add_neighbor(self, another_node):
-        self.neighbors.add(another_node)
+    def add_neighbor(self, neighbor_node: 'GraphNode'):
+        self.neighbors.append(neighbor_node)
 
 
 class Solution:
     def __init__(self):
-        self.word_node_dict = {}
-        self.cache_dict = {}
+        self.graph: Dict[str, GraphNode] = {}
 
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-        all_word_list = []
-        all_word_list.extend(wordList)
-        all_word_list.append(beginWord)
-
-        for word in all_word_list:
-            self.create_or_get_graph_node(word)
-            for another_word in all_word_list:
-                if another_word == word:
-                    continue
-                if self.can_convert(word, another_word):
-                    self.add_neighbor_for_both_word(word, another_word)
-
-        begin_word_node = self.word_node_dict[beginWord]
-        min_steps = self.dfs(begin_word_node, endWord, set())
-        if min_steps == 99999999:
+        if endWord not in wordList:
             return 0
-        return min_steps
 
-    def dfs(self, current_node: GraphNode, end_word: str, trace: Set) -> int:
-        if self.cache_dict.__contains__(current_node.val):
-            return self.cache_dict[current_node.val]
+        n = len(wordList)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if self.__is_neighbor_word__(wordList[i], wordList[j]):
+                    self.__build_relation__(wordList[i], wordList[j])
 
-        trace.add(current_node.val)
-        if current_node.val == end_word:
-            trace.remove(current_node.val)
-            self.cache_dict[current_node.val] = 1
+        minimum_steps = 2 ** 32 - 1
+        for i in range(n):
+            if self.__is_neighbor_word__(beginWord, wordList[i]):
+                steps = self.__dfs__(wordList[i], endWord, [])
+                if 0 < steps < minimum_steps:
+                    minimum_steps = steps
+
+        if minimum_steps == 2 ** 32 - 1:
+            return 0
+        return minimum_steps + 1
+
+    def __dfs__(self, begin_word: str, end_word: str, trace_stack: List[str]) -> int:
+        if begin_word == end_word:
             return 1
 
-        min_steps_from_current_node = 99999999
-        for neighbor in current_node.neighbors:
-            if trace.__contains__(neighbor.val):
+        trace_stack.append(begin_word)
+
+        minimum_steps = 2 ** 32 - 1
+        begin_node = self.__get_or_create_node__(begin_word)
+
+        for neighbor_node in begin_node.neighbors:
+            if neighbor_node.val in trace_stack:
                 continue
-            steps = self.dfs(neighbor, end_word, trace)
-            if steps + 1 < min_steps_from_current_node:
-                min_steps_from_current_node = steps + 1
 
-        trace.remove(current_node.val)
-        self.cache_dict[current_node.val] = min_steps_from_current_node
-        return min_steps_from_current_node
+            steps = self.__dfs__(neighbor_node.val, end_word, trace_stack)
+            if 0 < steps < minimum_steps:
+                minimum_steps = steps
 
-    def create_or_get_graph_node(self, val: str) -> GraphNode:
-        if not self.word_node_dict.__contains__(val):
-            self.word_node_dict[val] = GraphNode(val)
+        trace_stack.pop()
 
-        return self.word_node_dict[val]
+        if minimum_steps == 2 ** 32 - 1:
+            return -1
 
-    def can_convert(self, word1: str, word2: str) -> bool:
-        diff_count = 0
-        for i in range(len(word1)):
+        return minimum_steps + 1
+
+    def __build_relation__(self, val1: str, val2: str):
+        node1 = self.__get_or_create_node__(val1)
+        node2 = self.__get_or_create_node__(val2)
+        node1.add_neighbor(node2)
+        node2.add_neighbor(node1)
+
+    def __get_or_create_node__(self, val: str):
+        if val not in self.graph:
+            self.graph[val] = GraphNode(val)
+        return self.graph[val]
+
+    def __is_neighbor_word__(self, word1: str, word2: str):
+        n = len(word1)
+        diff_cnt = 0
+        for i in range(n):
             if word1[i] != word2[i]:
-                diff_count += 1
-                if diff_count > 1:
-                    return False
+                diff_cnt += 1
 
-        return True
-
-    def add_neighbor_for_both_word(self, word1: str, word2: str):
-        word1_node = self.create_or_get_graph_node(word1)
-        word2_node = self.create_or_get_graph_node(word2)
-        word1_node.add_neighbor(word2_node)
-        word2_node.add_neighbor(word1_node)
+        return diff_cnt == 1
 
 
 s = Solution()
-print(s.ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]))
-# print(s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]))
+print(s.ladderLength("qa", "sq", ["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"]))
