6b6ffe91303d2e98eb88ff3bc298d13d9b01af2c
poype <253186926@qq.com>
2024-03-16 12:22:54 +0800

    优化

--- solve.py
+++ solve.py
@@ -7,42 +7,41 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        if len(board) == 0:
-            return
-
-        m = len(board)
-        n = len(board[0])
-
+        m, n = len(board), len(board[0])
         flag = [[False for _ in range(n)] for _ in range(m)]
 
         for i in range(n):
             if board[0][i] == 'O' and not flag[0][i]:
-                self.dfs(0, i, board, flag)
+                self.__dfs__(board, 0, i, flag)
+
             if board[m - 1][i] == 'O' and not flag[m - 1][i]:
-                self.dfs(m - 1, i, board, flag)
+                self.__dfs__(board, m - 1, i, flag)
 
         for i in range(m):
             if board[i][0] == 'O' and not flag[i][0]:
-                self.dfs(i, 0, board, flag)
+                self.__dfs__(board, i, 0, flag)
             if board[i][n - 1] == 'O' and not flag[i][n - 1]:
-                self.dfs(i, n - 1, board, flag)
+                self.__dfs__(board, i, n - 1, flag)
 
         for i in range(m):
             for j in range(n):
-                if not flag[i][j]:
+                if board[i][j] == 'O' and not flag[i][j]:
                     board[i][j] = 'X'
 
-    def dfs(self, row: int, column: int, board: List[List[str]], flag: List[List[int]]):
+    def __dfs__(self, board: List[List[str]], row: int, column: int, flag: List[List[bool]]):
         flag[row][column] = True
 
-        if row > 1 and not flag[row - 1][column] and board[row - 1][column] == 'O':
-            self.dfs(row - 1, column, board, flag)
-        if row < len(board) - 1 and not flag[row + 1][column] and board[row + 1][column] == 'O':
-            self.dfs(row + 1, column, board, flag)
-        if column > 1 and not flag[row][column - 1] and board[row][column - 1] == 'O':
-            self.dfs(row, column - 1, board, flag)
-        if column < len(board[0]) - 1 and not flag[row][column + 1] and board[row][column + 1] == 'O':
-            self.dfs(row, column + 1, board, flag)
+        if column < len(board[0]) - 1 and board[row][column + 1] == 'O' and not flag[row][column + 1]:
+            self.__dfs__(board, row, column + 1, flag)
+
+        if row < len(board) - 1 and board[row + 1][column] == 'O' and not flag[row + 1][column]:
+            self.__dfs__(board, row + 1, column, flag)
+
+        if column > 0 and board[row][column - 1] == 'O' and not flag[row][column - 1]:
+            self.__dfs__(board, row, column - 1, flag)
+
+        if row > 0 and board[row - 1][column] == 'O' and not flag[row - 1][column]:
+            self.__dfs__(board, row - 1, column, flag)
 
 
 s = Solution()
