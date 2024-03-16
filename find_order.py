5c950f0638c056c2d9922962fd0bdb7b2caf7493
poype <253186926@qq.com>
2024-03-16 16:30:07 +0800

    如何使用Type Hints定义局部变量

--- find_order.py
+++ find_order.py
@@ -1,57 +1,57 @@
 # https://leetcode.cn/problems/course-schedule-ii/
 
-from typing import List
+from typing import List, Dict
 
-
-# 类似于BFS，拓扑排序
 
 class GraphNode(object):
-    def __init__(self, course: int):
-        self.course = course
-        self.post_courses = []
+    def __init__(self, val: int):
+        self.val = val
+        self.neighbors: List['GraphNode'] = []
+        self.in_degree = 0
 
-    def add_post_course(self, course_node):
-        self.post_courses.append(course_node)
+    def add_neighbor(self, neighbor_node: 'GraphNode'):
+        self.neighbors.append(neighbor_node)
 
 
 class Solution:
+    def __init__(self):
+        self.graph: Dict[int, GraphNode] = {}
+
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        if numCourses == 1:
-            return [0]
-
-        course_order_list = []
-
-        course_list = []
         for i in range(numCourses):
-            course_list.append(GraphNode(i))
+            self.__get_or_create_node__(i)
 
-        in_degree_list = [0 for _ in range(numCourses)]
-        for relation in prerequisites:
-            pre_course = course_list[relation[1]]
-            post_course = course_list[relation[0]]
-            pre_course.add_post_course(post_course)
-            in_degree_list[relation[0]] += 1
+        for item in prerequisites:
+            self.__build_relation__(item[1], item[0])
 
-        for i in range(len(in_degree_list)):
-            stack = []
-            if in_degree_list[i] == 0 and not course_order_list.__contains__(i):
-                stack.append(i)
-                while len(stack) > 0:
-                    study_course_num = stack.pop()
-                    course_order_list.append(study_course_num)
+        queue = []
+        while len(queue) < numCourses:
+            flag = False
+            for node in self.graph.values():
+                if node.in_degree == 0 and node.val not in queue:
+                    queue.append(node.val)
+                    for neighbor_node in node.neighbors:
+                        neighbor_node.in_degree -= 1
 
-                    post_courses = course_list[study_course_num].post_courses
-                    while len(post_courses) > 0:
-                        post_course = post_courses.pop()
-                        in_degree_list[post_course.course] -= 1
-                        if in_degree_list[post_course.course] == 0:
-                            stack.append(post_course.course)
+                    flag = True
 
-        for in_degree in in_degree_list:  # 有环返回空list
-            if in_degree > 0:
+            if not flag:
                 return []
-        return course_order_list
+
+        return queue
+
+    def __build_relation__(self, val1: int, val2: int):
+        node1 = self.__get_or_create_node__(val1)
+        node2 = self.__get_or_create_node__(val2)
+        node1.add_neighbor(node2)
+        node2.in_degree += 1
+
+    def __get_or_create_node__(self, val: int):
+        if val not in self.graph:
+            self.graph[val] = GraphNode(val)
+
+        return self.graph[val]
 
 
 s = Solution()
-print(s.findOrder(3, [[1, 0], [1, 2], [0, 1]]))
+print(s.findOrder(3, [[1, 0], [1, 2]]))
